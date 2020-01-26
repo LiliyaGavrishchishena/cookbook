56d907479123054ab2f5c903e228457189ac6f21
Liliya Gavrishchishena <Liliya.Gavrishchishena@gmail.com>
2020-01-26 22:30:31 +0200

    deploy on heroku

--- app.js
+++ app.js
@@ -19,11 +19,11 @@ app.use(express.json({ extended: true }));
 
 app.use('/', require('./routes/recipe.routes'));
 
-// app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+app.use('/', express.static(path.join(__dirname, 'client', 'build')));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 const PORT = config.get('port') || 8000;
 
